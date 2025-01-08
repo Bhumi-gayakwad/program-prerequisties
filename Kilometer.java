e4fd168416cf6047fc7460409d4134a5017d3275
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-08 13:41:05 +0530

    kilometers

---
+++ Kilometer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Kilometer{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int kilometers=sc.nextInt();
+double miles=kilometers * 0.621371;
+System.out.println(miles);
+}
+}
