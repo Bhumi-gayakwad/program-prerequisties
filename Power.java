08d4f0ea7e995a168fa683f676f6a6684492c1b6
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-08 13:18:52 +0530

    5th program

---
+++ Power.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Power{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+double base=sc.nextDouble();
+double exponent=sc.nextDouble();
+double power=Math.pow(base,exponent);
+System.out.println(power);
+}
+}
