05e5918ef8c2a15d75e71609aab3a5ed763ac80b
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-08 13:43:21 +0530

    Delete Conversion.java

--- Conversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Temperature{
-public static void main(String[] args){
- Scanner sc=new Scanner(System.in);
- double Celsius = sc.nextDouble();
- double farentite = (Celsius * 9/5) + 32;
- System.out.println("farentite = " + farentite);
- }
- }
