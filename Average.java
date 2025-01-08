f6d97f250d45875c2b55419cfaa2c8cfe5be1afd
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-08 13:23:19 +0530

    9th program'

---
+++ Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Average{
+public static void main(String[] args){
+ Scanner sc=new Scanner(System.in);
+ int x=sc.nextInt();
+ int y=sc.nextInt();
+ int z=sc.nextInt();
+ double average=(x+y+z)/3;
+ System.out.println(average);
+ }
+ }
